c0baf3407913ca8967bb47720c80656583a85151
tranvanmanh9325 <manhtrana1k45tl@gmail.com>
2025-04-08 14:00:11 +0700

    Add .gitattributes to handle line endings

---
+++ map/src/GameLauncher.java
@@ -0,0 +1,25 @@
+package map.src;
+
+// Import
+import javax.swing.JFrame;
+
+public class GameLauncher {
+
+    public static void main (String[] args) {
+
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("2D RPG Game");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gamePanel.startGameThread();
+    }
+}
---
+++ map/src/KeyHandler.java
@@ -0,0 +1,23 @@
+package map.src;
+
+// Import
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int code = e.getKeyCode();
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
